cc67024bf4d438b96a06426e5798520c8f65819c
ehtiram <ehtirammeherremov@hotmail.com>
2022-06-02 11:46:34 +0400

    first commit

---
+++ Pustok/Pustok/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.DAL;
+using Pustok.Models;
+using Pustok.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Pustok.Controllers
+{
+    public class HomeController : Controller
+    {
+         private AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+             List<Slider> sliders = _context.Sliders.OrderBy(x=>x.Order).ToList();
+            return View();
+        }
+    }
+}
